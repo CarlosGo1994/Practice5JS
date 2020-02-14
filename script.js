f17804ba9903350781d02171a745691ba233dadc
CarlosGo1994 <charls21ronald@gmail.com>
2020-02-13 23:34:11 -0600

    Simple example about basic operators and data types. Some functions like String, Number,parseInt, parsefloat.

---
+++ script.js
@@ -0,0 +1,39 @@
+'use strict'
+
+//Two numbers are declared
+let number1 = 7;
+let number2 = 12;
+
+//Basic Operators
+let sum = number1 + number2;
+let rest = number1 - number2;
+let multiplication = number1 * number2;
+let divison = number1 / number2;
+
+//Output 
+alert("The sum is: "+ sum);
+alert("The rest is: "+ rest);
+alert("The multiplication is: "+ multiplication);
+alert("The division is: "+ divison);
+
+//Basic Data types
+
+let integer_number = 44;
+let text_string = "Hey 'What's up' man ";
+let true_or_false = true;//this can be true or false but you can use digits like zero or one.
+let false_number = "25";//this number is taken it like string
+
+
+//Output in console
+console.log(integer_number + ' concatenated text');
+console.log(integer_number+20);
+console.log(true_or_false);
+console.log(text_string);
+
+//Functions 
+console.log(String(integer_number)+5);//This function convert to string the integer variable
+console.log(Number(false_number)+5);//This function convert to number the variable
+console.log(parseInt(false_number)+7);//This fuction convert to integer the variable
+console.log(parseFloat(false_number)+5.85);//This fuction convert to float the variable
+
+
